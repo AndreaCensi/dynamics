b403beb6e6de067b0d7a4cceae9c7d568344178d
Andrea Censi <andrea@cds.caltech.edu>
2011-07-15 01:44:11 -0700

    reorganization

---
+++ src/vehicles_dynamics/specializations/todo/differential_drive.py
@@ -0,0 +1,18 @@
+#from . import Dynamics
+#
+#class DifferentialDrive(Dynamics):
+#    
+#    def __init__(self, max_angular_velocity):
+#        Dynamics.__init__(self, state_space=SE2)
+#        self.se2_dynamics = SE2Dynamics()
+#    
+#    def _check_commands(self, commands):
+#        commands = np.array(commands)
+#        check(commands, 'array[2],finite')
+#        
+#    def _integrate(self, state, commands, dt):
+#        driving = commands[0]
+#        steering = commands[1]
+#        commands = [driving, steering, 0]
+#        self.s
+#        
